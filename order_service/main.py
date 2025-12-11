7fc76170b9d9ef0d1cb177dcd6bd30467a8e896f
Maksym <maksym.pavliuk@nure.ua>
2025-12-11 22:02:54 +0200

    test form max 6

--- order_service/main.py
+++ order_service/main.py
@@ -1,1 +1,6 @@
 print("Hello, World from Max")
+print("Hello, World from Max")
+print("Hello, World from Max")
+print("Hello, World from Max")
+print("Hello, World from Max")
+print("Hello, World from Max")
