963d0e9ed523da23dccfaee2640d038b09843424
Maksym <maksym.pavliuk@nure.ua>
2025-12-12 23:27:41 +0200

    added base api structure

--- order_service/main.py
+++ order_service/main.py
@@ -1,6 +1,32 @@
-print("Hello, World from Max")
-print("Hello, World from Max")
-print("Hello, World from Max")
-print("Hello, World from Max")
-print("Hello, World from Max")
-print("Hello, World from Max")
+from fastapi import FastAPI, APIRouter
+
+app = FastAPI()
+router = APIRouter(prefix="/order")
+
+
+@router.get("/user/{user_id}")
+def getAllUserOrders(user_id: int):
+    pass
+
+
+@router.get("/{id}")
+def findOrderById(id: int):
+    pass
+
+
+@router.post("/")
+def createOrder(order: dict):
+    pass
+
+
+@router.put("/{id}/status")
+def updateOrderStatus(id: int, status: str):
+    pass
+
+
+@router.delete("/{id}")
+def cancelOrder(id: int):
+    pass
+
+
+app.include_router(router)
